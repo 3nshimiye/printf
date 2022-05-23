09835d5121869f55f1199822c2dab1a0f922170f
Emmanuel Nshimiyimana <3nshimiye@gmail.com>
2022-05-23 13:11:54 -0700

    0x11. C - printf

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x11. C - printf, a Project to be done in teams of 2 people and here is Emmanuel Nshimiyimana and Erica Ngabire.
+0x11. C - printf, a Project to be done in teams of 2 people and here is Emmanuel Nshimiyimana and Wills Ajiboye.
--- main.c
+++ main.c
@@ -14,14 +14,14 @@ int main(void)
     unsigned int ui;
     void *addr;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
+    len = _printf("Let's try a simple sentence.\n");
+    len2 = printf("Let's try tthis one too!.\n");
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
+    _printf("Negative:[%d]\n", -76254);
+    printf("Negative:[%d]\n", -76254);
     _printf("Unsigned:[%u]\n", ui);
     printf("Unsigned:[%u]\n", ui);
     _printf("Unsigned octal:[%o]\n", ui);
@@ -30,8 +30,8 @@ int main(void)
     printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
+    _printf("String:[%s]\n", "this is a string !");
+    printf("String:[%s]\n", "this is a string !");
     _printf("Address:[%p]\n", addr);
     printf("Address:[%p]\n", addr);
     len = _printf("Percent:[%%]\n");
